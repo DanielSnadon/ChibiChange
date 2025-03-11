e173db63d363a97b54d5af6996e21d877cec7943
uncommonde4th <uncommonde4th@gmail.com>
2025-03-11 14:53:27 +0300

    flashFix

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,6 @@ def register_routes(app):
             user = User.query.filter_by(username=form.username.data).first()
             if user and user.check_password(form.password.data):
                 login_user(user)
-                flash('Login successful!', 'success')
                 return redirect(url_for('home'))  # Перенаправление на домашнюю страницу
             else:
                 flash('Invalid username or password', 'danger')
